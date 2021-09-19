119e27709a3a96d6300cf77848b88e40d0d2aabb
Dylan <90397861+caplox@users.noreply.github.com>
2021-09-19 22:19:55 +0200

    Delete btc.py

--- btc.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# Getting BTC price from coindesk using response module
-import requests
-response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-data = response.json()
-btc_price = data["bpi"]["USD"]["rate"]
-f = float(btc_price.replace(',',''))
