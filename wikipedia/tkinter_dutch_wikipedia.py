944cfefbec0dbf18829d234816e3e0c74c5a8583
Dylan <90397861+caplox@users.noreply.github.com>
2021-09-22 14:20:04 +0200

    Create tkinter_dutch_wikipedia.py

---
+++ wikipedia/tkinter_dutch_wikipedia.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup
+import urllib.request
+from tkinter import Tk, Text, Entry, Button, END, INSERT
+from urllib.error import HTTPError
+
+root = Tk()
+root.title("Dutch Wikipedia")
+
+
+def conf_click():
+    global space1
+    word = input_field.get()
+    try:
+        if " " in word:
+            word = word.replace(" ", "_")
+        if word[0].islower:
+            word = word[0].upper() + word[1:]
+        if "_" in word:
+            space1 = (word.index("_") + 1)
+        if "_" in word:
+            if word[space1].islower:
+                word = word[:space1] + word[space1].upper() + word[space1 + 1:]
+
+        content = urllib.request.urlopen('https://nl.wikipedia.org/wiki/' + word)
+        read_content = content.read()
+        soup = BeautifulSoup(read_content, 'html.parser')
+        pAll = soup.find_all('p')
+        answer = str(pAll[0].text)
+
+        answer = answer.replace("[1]", "")
+        answer = answer.replace("[2]", "")
+        answer = answer.replace("[3]", "")
+        answer = answer.replace("[4]", "")
+        answer = answer.replace("[5]", "")
+        answer = answer.replace("[6]", "")
+        answer = answer.replace("[7]", "")
+        answer = answer.replace("[8]", "")
+        answer = answer.replace("[9]", "")
+        answer = answer.replace("[10]", "")
+        answer = answer.replace("[11]", "")
+        answer = answer.replace("(info / uitleg);", ":")
+
+        if len(answer) == len(word) + 21:
+            output_field.delete('1.0', END)
+            output_field.insert(INSERT, word + " kan verwijzen naar meerdere dingen. Probeer wat specifieker te zijn.")
+        elif str(pAll[0].text) != len(word) + 21:
+            output_field.delete('1.0', END)
+            output_field.insert(INSERT, answer)
+    except HTTPError:
+        output_field.delete("1.0", END)
+        output_field.insert(INSERT, "An article about this doesn\'t exist yet.")
+
+
+input_field = Entry(root)
+output_field = Text(root)
+
+cf_input_field = Button(root, text="Confirm?", command=lambda: conf_click())
+
+input_field.grid(row=0, column=0, padx=30, pady=2)
+output_field.grid(row=1, column=0, padx=100, pady=50, columnspan=5, ipady=50)
+
+cf_input_field.grid(row=0, column=1)
+
+root.mainloop()
